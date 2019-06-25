7ada94faa2ad8273e80dfed51a814d54ecd838dd
v5555749 <v555574@ya.ru>
2019-06-25 08:59:53 +0400

    Добавил отправлялку почты

---
+++ src/main/java/com/itplace/emailmanager/EmailManager.java
@@ -0,0 +1,13 @@
+package com.itplace.emailmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailManager {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailManager.class, args);
+
+    }
+}
