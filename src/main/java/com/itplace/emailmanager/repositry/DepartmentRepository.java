162e6ee377012fa4a9d93bdc3e19b6a9ae50843a
v5555749 <v555574@ya.ru>
2019-07-02 15:22:37 +0400

    импорт/экспорт контактов из csv

--- src/main/java/com/itplace/emailmanager/repositry/DepartmentRepository.java
+++ src/main/java/com/itplace/emailmanager/repositry/DepartmentRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DepartmentRepository extends LongJpaRepository<Department> {
     boolean existsByNameEqualsIgnoreCase(String name);
+
+    Department findByNameEquals(String name);
 }
--- src/main/java/com/itplace/emailmanager/util/AddresseeImportExport.java
+++ src/main/java/com/itplace/emailmanager/util/AddresseeImportExport.java
@@ -1,10 +1,14 @@
 package com.itplace.emailmanager.util;
 
 import com.itplace.emailmanager.domain.Addressee;
+import com.itplace.emailmanager.service.AddresseeService;
+import com.itplace.emailmanager.service.DepartmentService;
+import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 import com.opencsv.bean.*;
 import com.opencsv.exceptions.CsvDataTypeMismatchException;
 import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
@@ -16,36 +20,46 @@ import java.util.List;
 
 @Component
 public class AddresseeImportExport {
+    @Autowired
+    private DepartmentService departmentService;
 
     public List<Addressee> importFromFile(String stringPath) {
-        Path path = Paths.get(stringPath);
-
-        if (path.toFile().canRead()) {
-            ColumnPositionMappingStrategy columnPositionMappingStrategy = new ColumnPositionMappingStrategy();
-            columnPositionMappingStrategy.setType(Addressee.class);
-            CsvToBean csvToBean = null;
-            try (Reader reader = Files.newBufferedReader(path)) {
-                 csvToBean = new CsvToBeanBuilder(reader).withType(Addressee.class)
-                        .withMappingStrategy(columnPositionMappingStrategy).build();
-            } catch (IOException e) { // игнорируем, т.к. возвращаем пустой список в случае любой неудачи
+        List<Addressee> addresseeList = new ArrayList<>();
+        File file = new File(stringPath);
+        List<String[]> linesList = new ArrayList<>();
+        if (file.canRead()) {
+            try (FileReader fileReader = new FileReader(file)){
+                CSVReader csvReader = new CSVReader(fileReader);
+                linesList = csvReader.readAll();
+                csvReader.close();
+            } catch (IOException e) {
+                return addresseeList;
             }
-            if (csvToBean != null) return csvToBean.parse();
+            linesList.forEach(line -> {
+                Addressee addressee = new Addressee();
+                addressee.setEmail(line[0]);
+                addressee.setName(line[1]);
+                addressee.setDepartment(departmentService.findByNameEquals(line[2]));
+                addresseeList.add(addressee);
+            });
         }
-        return new ArrayList<>();
+        return addresseeList;
     }
 
-    public boolean exportToFile(List<Addressee> addresseeList, String stringPath) throws IOException {
+    public void exportToFile(List<Addressee> addresseeList, String stringPath) throws IOException {
         File outputFile = new File(stringPath);
-
         if (outputFile.createNewFile()) {
-            try (Writer writer = new FileWriter(outputFile)) {
-                StatefulBeanToCsv statefulBeanToCsv = new StatefulBeanToCsvBuilder(writer)
-                        .withSeparator(CSVWriter.DEFAULT_SEPARATOR).build();
-                statefulBeanToCsv.write(addresseeList);
-            } catch (IOException | CsvDataTypeMismatchException | CsvRequiredFieldEmptyException e) {
-                return false;
-            }
+            List<String[]> linesToWrite = new ArrayList<>();
+            CSVWriter csvWriter = new CSVWriter(new FileWriter(outputFile));
+            addresseeList.forEach(addressee -> {
+                List<String> line = new ArrayList<>();
+                line.add(addressee.getEmail());
+                line.add(addressee.getName());
+                line.add(addressee.getDepartment().getName());
+                linesToWrite.add(line.toArray(new String[0]));
+            });
+            csvWriter.writeAll(linesToWrite);
+            csvWriter.close();
         }
-        return false;
     }
 }
